eb646ca83f5d82d0f2a668e3fc96ce8929c10d03
aditya11011 <adityaswaraj101@gmail.com>
2025-02-17 01:42:06 +0530

    added basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App /></BrowserRouter>
 
   </StrictMode>,
